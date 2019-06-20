a579b2ba263b92d02b98704d9c73a967c5f4217e
Anand Lok <34028605+anandlok20@users.noreply.github.com>
2019-06-21 01:05:00 +0530

    Add files via upload

---
+++ ConsoleJumpString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleJumpString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nEnter the String:");
+            string str = Console.ReadLine();
+            char[] c = str.ToCharArray();
+            char[] c1=new char[c.Length];
+            int l = 0;
+            for (int i=2;i<c.Length;i++)
+            {
+                c1[l++] = c[i];
+            }
+            string str2 = new string(c1);
+            Console.WriteLine("The string is "+str+"\nString after alter is: "+str2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleSumArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nEnter the size of the array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[size];
+            Console.WriteLine("\nEnter the element of the array:");
+            for(int i=0;i<array.Length;i++)
+            {
+                int ele = Convert.ToInt32(Console.ReadLine());
+                array[i] = ele;
+            }
+            Console.WriteLine("\nThe array Elements are:");
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]+"\t");
+                sum = sum + array[i];
+            }
+            Console.WriteLine("\nThe sum of the array:"+sum);
+            Console.ReadKey();
+        }
+    }
+}
