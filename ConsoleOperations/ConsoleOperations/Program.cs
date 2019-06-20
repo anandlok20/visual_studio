d99e5fc149e73be76d140bf294fe0d628636ff16
Anand Lok <34028605+anandlok20@users.noreply.github.com>
2019-06-20 22:45:19 +0530

    Add files via upload

---
+++ ConsoleOperations/ConsoleOperations/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean loop = true;
+            Console.WriteLine("\n(For string operations enter size 0)\nEnter the size of the array");
+            int s = Convert.ToInt32(Console.ReadLine());
+            operations op = new operations();
+            int[] array = new int[s];
+            Console.WriteLine("Enter Elements:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                array[i] = a;
+            }
+            string print;
+            while (loop)
+            {
+                Console.WriteLine("\nEnter your choice\n1.Find Maximum and Minimum\n2.Swaping first and last number\n3.Check Prime\n4.Find no of words in a given string\n5.Find count of integer in a given string\n6.Convert the given string into its opposite case\n7.Find count of positive and negative integer\n8.Find highest and lowest and swap positions\n9.Find given string is palindrome\n10.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        print = op.maxMin(array);
+                        Console.WriteLine(print);
+                        break;
+                    case 2:
+                        Console.WriteLine("The orignal array is:");
+                        op.print(array);
+                        Console.WriteLine("The swapped array is:");
+                        int[] a = op.swapnum(array);
+                        op.print(a);
+                        break;
+                    case 3:
+                        Console.WriteLine("The orignal array is:");
+                        op.print(array);
+                        Console.WriteLine("The prime no array is:");
+                        op.primeno(array);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter a String");
+                        string str = Console.ReadLine();
+                        string[] str1 = str.Split(' ');
+                        Console.WriteLine("No of words in a given string:");
+                        Console.Write(str1.Length);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter a String");
+                        string str2 = Console.ReadLine();
+                        Console.WriteLine("No of integer's in the given string:");
+                        op.intcount(str2);
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter a String");
+                        string str3 = Console.ReadLine();
+                        Console.WriteLine("No of integer's in the given string:");
+                        StringBuilder sb = op.upplow(str3);
+                        Console.WriteLine(sb);
+                        break;
+                    case 7:
+                        Console.WriteLine("The orignal array is:");
+                        op.print(array);
+                        op.posneg(array);
+                        break;
+                    case 8:
+                        Console.WriteLine("The orignal array is:");
+                        op.print(array);
+                        int[] res = op.swapmm(array);
+                        Console.WriteLine("The swaped array is:");
+                        op.print(res);
+                        break;
+                    case 9:
+                        Console.WriteLine("Enter a String");
+                        string str4 = Console.ReadLine();
+                        op.palin(str4);
+                        break;
+                    case 10:
+                        loop = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input!!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleOperations/ConsoleOperations/operations.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOperations
+{
+    class operations
+    {
+        int max, min;
+        public operations() { Console.WriteLine("In Constructor.."); }
+
+        public void print(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+                Console.Write(a[i]+"\t");
+            Console.WriteLine("\n");
+        }
+        public string maxMin(int[] arr)
+        {
+            max = arr[0];
+            min = arr[0];
+            for(int i=1;i<arr.Length;i++)
+            {
+                if (arr[i]>=max)
+                {
+                    max = arr[i];
+                }
+                if(arr[i]<min)
+                {
+                    min = arr[i];
+                }
+            }
+            return "Maximum :" + max + "\nMinimum :" + min;
+        }
+
+        public int[] swapnum(int[] arr)
+        {
+            arr[0] = arr[0] + arr[arr.Length-1];
+            arr[arr.Length - 1] = arr[0] - arr[arr.Length - 1];
+            arr[0] = arr[0] - arr[arr.Length - 1];
+            return arr;
+        }
+
+        public void primeno(int[] arr)
+        {
+            int j, p;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                j = 2;
+                p = 1;
+                while (j < arr[i])
+                {
+                    if (arr[i] % j == 0)
+                    {
+                        p = 0;
+                        break;
+                    }
+                    j++;
+                }
+                if (p == 1)
+                {
+                    Console.Write(arr[i] + "\t");
+                }
+            }
+        }
+
+        public void intcount(string str)
+        {
+            char[] c = str.ToCharArray();
+            int count=0;
+            for (int i = 0; i < c.Length; i++)
+            {
+                if(c[i]>'0' && c[i]<'9')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("No of integer: "+count);
+        }
+
+        public StringBuilder upplow(string str)
+        {
+            StringBuilder sb = new StringBuilder(str);
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if(sb[i]>='a' && sb[i]<='z')
+                {
+                    sb[i] = (char)(sb[i] - 32);
+                }
+                else if (sb[i] >= 'A' && sb[i] <= 'Z')
+                {
+                    sb[i] = (char)(sb[i] + 32);
+                }
+            }
+            return sb;
+        }
+
+        public void posneg(int[] a)
+        {
+            int pos=0, neg=0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if((a[i])>0)
+                {
+                    pos++;
+                }
+                if ((a[i]) < 0)
+                {
+                    neg++;
+                }
+            }
+            Console.WriteLine("\nno of +ve integer: "+pos+"\nno of -ve integer: "+neg);
+        }
+
+        public int[] swapmm(int[] a)
+        {
+            maxMin(a);
+            int p1=0, p2=0, temp=0;
+            for(int i=0;i<a.Length;i++)
+            {
+                if (a[i] == max)
+                    p1 = i;
+                if (a[i] == min)
+                    p2 = i;
+            }
+            temp = a[p1];
+            a[p1] = a[p2];
+            a[p2] = temp;
+            return a;
+        } 
+
+        public void palin(string s)
+        {
+            string r=null;
+            for (int i = s.Length - 1; i >= 0; i--) 
+            {
+                r += s[i].ToString();
+            }
+            if (r == s)  
+            {
+                Console.WriteLine("String is Palindrome \norignal string :" + s + "\nreverse string is :" + r);
+            }
+            else
+            {
+                Console.WriteLine("String is not Palindrome \norignal string :"+s+"\nreverse string is :"+r);
+            }
+        }
+    }
+}
